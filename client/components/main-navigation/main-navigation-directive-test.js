cad43f8037cbfbc0dc76c5951298f770955854ef
Randall Meeker <randallmeeker@gmail.com>
2016-03-09 12:23:23 -0600

    add main navigation

---
+++ client/components/main-navigation/main-navigation-directive-test.js
@@ -0,0 +1,8 @@
+describe( 'main-navigation directive', () => {
+  beforeEach( module( 'sails-gulp-seed' ) )
+
+  it( 'should work', () => {
+    expect( 1 ).toBe( 1 )
+  } )
+
+} )
---
+++ client/components/main-navigation/main-navigation-directive.js
@@ -0,0 +1,38 @@
+// Directive navigationMain TODO
+(function(){
+  angular.module( 'sails-gulp-seed' )
+    .directive( 'mainNavigation', () => {
+      return {
+        templateUrl: '/assets/templates/navigation-main.html',
+        scope: {
+        },
+        link: function( /* $scope, $element, $attributes */ ) {},
+        controllerAs: 'navigationMain',
+        bindToController: true,
+        controller: function( ) {
+          // Instead of using scope, you should reference the variable 'navigationMain'
+          // Comment this line out if NO directive variables are set so it will pass linting
+          var navigationMain = this;
+
+          navigationMain.items = [
+            {
+              icon: 'today',
+              title: 'Calendar',
+              route: 'home'
+            },
+            {
+              icon: 'supervisor_account',
+              title: 'Clients',
+              route: 'home'
+            },
+            {
+              icon: 'face',
+              title: 'Staff',
+              route: 'home'
+            }
+          ]
+
+        }
+      }
+  } )
+})()
